a801ef97593d22e7edf2980bb34b907332f91c98
BarrigueteHector <hectorrodjls@gmail.com>
2023-07-28 21:21:34 -0600

    Day07 listo

---
+++ Day07/app.js
@@ -0,0 +1,10 @@
+// Obtenemos la ruta actual de la página
+var currentPath = window.location.pathname;
+
+// Agregamos la clase "active" al enlace correspondiente según la página actual
+var links = document.querySelectorAll('a');
+links.forEach(function(link) {
+    if (link.getAttribute('href') === currentPath) {
+        link.classList.add('active');
+    }
+});
